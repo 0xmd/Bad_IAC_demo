342c4f4a217ba773936133885da62362409abeec
0xmd <max.d@outlook.com>
2022-10-10 16:31:30 +0800

    Added working Config rule via TF

--- ConfigRules.tf
+++ ConfigRules.tf
@@ -1,4 +1,4 @@
-resource "aws_config_config_rule" "restricted_ssh" {
+resource "aws_config_config_rule" "restricted-ssh" {
   name        = "restricted-ssh"
   description = "Checks whether security groups that are in use disallow unrestricted incoming SSH traffic."
 
@@ -6,4 +6,7 @@ resource "aws_config_config_rule" "restricted_ssh" {
     owner             = "AWS"
     source_identifier = "INCOMING_SSH_DISABLED"
     }
+  scope {
+    compliance_resource_types = ["AWS::EC2::SecurityGroup"]
+  }
 }
